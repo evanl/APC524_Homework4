b55b6b34ee48d7821eca6e8af01855faf1d3dfb9
Evan Leister <evanl@princeton.edu>
2012-11-15 11:10:41 -0500

    added OMP files for real, they work.

---
+++ heat_omp.cc
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <math.h>
+#include <vector>
+#include "heat_functions.h"
+
+using std::vector;
+
+const double PI = 3.14159265359;
+
+int main (int argc, const char *argv[] ){
+
+  time_t beginTime; 
+  beginTime = time(NULL);
+  // process command line argument. 
+  if (argc != 3){
+    printf( "Invalid input, please specify nx \n ");
+    return 1;
+  }
+  const int nx = atoi(argv[1]);
+  const int nthreads = atoi(argv[2]);
+
+  // initialize physical constants. 
+  const double dx = PI / (nx - 1 );
+  const double k = 0.25 ;//* pow(10.0,-6.0);
+  const double dt = dx * dx / (4.0 * k);
+  const double tmax = 0.5 * PI * PI / k;
+  const int tsteps = ceil(tmax/dt);
+
+  std::cout << "dx = " << dx << std::endl;
+  std::cout << "k  = " << k << std::endl;
+  std::cout << "tmax = " << tmax << std::endl;
+  std::cout << "dt = " << dt << std::endl;
+  std::cout << "tsteps = " << tsteps << std::endl;
+  std::cout << "nthreads = " << nthreads << std::endl;
+
+  doubleArray arr1, arr2;
+  arr1 = doubleArrayCreate(nx);
+  arr2 = doubleArrayCreate(nx);
+ 
+  heat_initialize(arr1, nx, dx);
+  heat_initialize(arr2, nx, dx); 
+
+  // solve equation. 
+  heat_solve(arr1, arr2, k, dx, dt, nx, tsteps, nthreads);
+
+  double taverage;
+
+  taverage = heat_average(arr2, nx); 
+
+  std::cout << "t_average = " << taverage << std::endl;
+
+  std::cout << "time to compute [seconds] = "; 
+  std::cout << ((double)(time(NULL)-beginTime));
+  std::cout << std::endl;
+
+  
+  // Write the file to output.dat
+  heat_write_contour( arr2, dx, nx );
+
+  doubleArrayRemove(arr1 ,nx);
+  doubleArrayRemove(arr2 ,nx );
+
+  return 0;
+}
---
+++ heat_omp_functions.cc
@@ -0,0 +1,153 @@
+#include <omp.h>
+#include "heat_functions.h"
+
+using std::ofstream;
+
+doubleArray doubleArrayCreate(int nx){
+
+  double **arr;
+
+  arr = new double*[nx];
+  for (int i=0; i < nx; ++i){
+    arr[i] = new double[nx];
+  }
+
+  return arr;
+}
+
+int doubleArrayRemove(doubleArray &arr1, int nx){
+
+  for (int i=0; i < nx; ++i ){
+   delete [] arr1[i];
+  }
+  delete [] arr1; 
+
+  return 0;
+}
+
+int heat_initialize(doubleArray &arr1, const int nx, const double dx){
+
+  for (int i = 0 ; i < nx ; ++i ){
+    for ( int j = 0 ; j < nx; ++j){
+      // evaluates boundary conditions. 
+      if ( j == 0 ){
+        arr1[i][j] = cos(i * dx) * cos(i*dx);
+      }else if ( j == (nx-1)) {
+        arr1[i][j] = sin( i * dx) * sin(i *dx);
+      } else{
+      arr1[i][j] = 0.0;
+      }
+    }
+  }
+
+  return 0;
+}
+
+int heat_solve( doubleArray &arr1, doubleArray &arr2, double k,
+   double dx, double dt, int nx, int tsteps,  int nthreads  ) {
+
+  int count = 0;
+  for (int i = 0 ; i < tsteps; i++){
+    count++;
+    if ( i % 2 == 0 ) {
+      heat_step(arr1, arr2, k, dx, dt, nx, nthreads);
+    }else {
+      heat_step(arr2, arr1, k, dx, dt, nx, nthreads);
+    }
+    if ((arr2[1][1] != arr2[1][1]) || (arr1[1][1] != arr1[1][1])){ 
+      std::cout << "diverged after " << count << " iterations";
+      std::cout << std::endl;
+      exit(1);
+    }
+  }
+  return 0;
+}
+
+
+int heat_step(const doubleArray &current, doubleArray &next , const double &k,
+   const double &dx, const double &dt, const int &nx,  int nthreads  ) {
+
+  int chunk = nx / nthreads;
+
+  #pragma omp parallel shared(current,next,dx,dt,nx,nthreads)   
+    {
+      #pragma omp for schedule(dynamic,chunk) nowait
+      for (int i = 0; i < nx; i++){
+        for (int j = 0; j <nx; j++){
+          double w, e, s, n, c;
+
+          if ((j!=0) && (j != (nx-1))){
+
+            if (i==0){
+              w = current[nx-1][j];
+            }else{
+              w = current[i-1][j];
+            }
+            if (i == (nx-1)){
+              e = current[0][j];
+            }else{
+              e = current[i+1][j];
+            }
+            n = current[i][j+1];
+            s = current[i][j-1];
+            c = current[i][j];
+
+            //std::cout << i << " " << j << std::endl;
+            //std::cout << w << " " << e << " " << n << " ";
+            //std::cout << s << " " << c << " " << std::endl;
+            next[i][j] = c + dt * k * ( w + e + n + s - 4 * c) / (dx * dx);
+          }
+        }
+      }
+    } // end of parallel section.
+  return 0;
+}
+
+
+double heat_average(const doubleArray &arr, int nx) {
+
+  double taverage;
+  double sumT = 0.0;
+  double N = nx * nx ;
+
+  for( int i=0 ; i < nx ; i++ )
+  {
+    for( int j=0 ; j < nx  ; j++ )
+    {
+      sumT = sumT + arr[i][j];
+    }
+    
+  }
+  return taverage = sumT / N; 
+}
+
+int heat_write_contour( const doubleArray &arr2, double dx, int nx ){
+
+  // writes files to be plotted 
+  ofstream output;
+  output.open("output.dat");
+  for (int i = 0 ; i < nx ; ++i ){
+    for ( int j = 0 ; j < nx; ++j){
+      output <<  i * dx << " ";
+      output <<  j * dx << " ";
+      output << arr2[i][j] << std::endl;
+      if( i == (nx-1) && j == (nx-1)){
+        output << std::endl;
+      }
+    }
+  }
+  output.close();
+  
+  
+  // forms a contour plot of the output data. 
+  //FILE *in;
+  //char buff[512];
+  //if(!( in = popen("gnuplot heat_plot.gnu","r"))){
+  //  exit(1);
+  //}
+  //while(fgets(buff,sizeof(buff),in)!=NULL){
+  //  std::cout << buff;
+  // }
+  //pclose(in);
+  return 0;
+}
